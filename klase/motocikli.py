f7e3ede12250210b31ee7b50fd809d5dd00d699f
mateabaric <131155225+mateabaric@users.noreply.github.com>
2024-10-24 09:56:43 +0200

    Update motocikli.py

--- klase/motocikli.py
+++ klase/motocikli.py
@@ -12,33 +12,33 @@ class Sportski(Motocikl):
     def __init__(self, brzina, cijena, snaga_motora, marka):
         
         super().__init__(brzina, cijena)
-        self.r=vrsta_robe
-        self.p=prikolica
+        self.s=snaga_motora
+        self.m=marka
     def __repr__(self):
-        return f'Šleper dužina: {self.d} namjena: {self.n} vrsta_robe:{self.r} prikolica:{self.p}'
+        return f'Sportski motocikl brzina: {self.b} cijena: {self.c} snaga_motora:{self.s} marka:{self.m}'
     def __str__(self):
         return self.__repr__()
 
 
-class Autotransporter(Kamion):
-    def __init__(self, težina, namjena, broj_katova, platforma):
+class Cruiser(Motocikl):
+    def __init__(self, brzina, cijena, visina, godina_proizvodnje):
        
-        super().__init__(težina, namjena)
-        self.k=broj_katova
-        self.plat=platforma
+        super().__init__(brzina, cijena)
+        self.v=visina
+        self.g=godina_proizvodnje
     def __repr__(self):
-        return f'Autotransporter dužina: {self.d} namjena: {self.n} broj_katova:{self.k} platforma:{self.plat}'
+        return f'Cruiser motocikl brzina: {self.b} cijena: {self.c} visina:{self.v} godina_proizvodnje:{self.g}'
     def __str__(self):
         return self.__repr__()
 
 
-class Cisterna(Kamion):
-    def __init__(self, broj_kotača, namjena, tekućina, kapacitet):
+class Skuter(Motocikl):
+    def __init__(self, brzina, cijena, težina, boja):
  
-        super().__init__(težina, namjena)
-        self.tek=tekućina
-        self.kap=kapacitet
+        super().__init__(brzina, cijena)
+        self.t=težina
+        self.boja=boja
     def __repr__(self):
-        return f'Cisterna dužina: {self.d} namjena: {self.n} tekućina:{self.tek} kapacitet:{self.kap}'
+       return f'Skuter brzina: {self.b} cijena: {self.c} težina:{self.t} boja:{self.boja}'
     def __str__(self):
         return self.__repr__()
