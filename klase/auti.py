2532b1842992ec9e0d526dfa73360571a70ae4e8
mateabaric <131155225+mateabaric@users.noreply.github.com>
2024-10-24 09:16:51 +0200

    Update auti.py

--- klase/auti.py
+++ klase/auti.py
@@ -1,46 +1,44 @@
 class Auto:
-    def __init__(self,naziv, kapacitet):
-        '''klasa za aktivnosti'''
-        self.n=naziv
-        self.k=kapacitet
+    def __init__(self,broj_kotača, namjena):
+        
+        self.k=broj_kotača
+        self.n=namjena
     def __repr__(self):
-        return 'Naziv (kapacitet):'+self.n+'( '+self.k + ')'
+        return 'broj kotača (namjena):'+self.k+'( '+self.n + ')'
     def __str__(self):
         return self.__repr__()
 
 class Pickup(Auto):
-    def __init__(self, naziv, kapacitet,grad, cijena):
-        '''nasljeđuje klasu Aktivnost'''
-        super().__init__(naziv,kapacitet)
-        self.g=grad
-        self.c=cijena
+    def __init__(self, broj_kotača, namjena, broj_vrata, kapacitet):
+        
+        super().__init__(broj_kotača, namjena)
+        self.v=broj_vrata
+        self.kap=kapacitet
     def __repr__(self):
-        return f'Razgledavanje:{self.n} ({self.k},{self.g},{self.c})'
+        return f'Pickup broj  kotača: {self.k} namjena: {self.n} broj vrata:{self.v} kapacitet:{self.kap}'
     def __str__(self):
         return self.__repr__()
 
 
 class SUV(Auto):
-    def __init__(self, naziv, kapacitet,polaziste, odrediste, cijena):
-        '''nasljeđuje klasu Ucenik, ali dodaje rječnik matura koji ima oblik {'naziv predmeta na maturi':'razina A ili B'}'''
-        super().__init__(naziv,kapacitet)
-        self.p=polaziste
-        self.o=odrediste
+    def __init__(self, broj_kotača, namjena, cijena, visina):
+       
+        super().__init__(broj_kotača, namjena)
         self.c=cijena
+        self.vis=visina
     def __repr__(self):
-        return f'Izlet:{self.n} ({self.k},{self.p}-{self.o},{self.c})'
+        return f'SUV broj  kotača: {self.k} namjena: {self.n} cijena:{self.c} visina:{self.vis}'
     def __str__(self):
         return self.__repr__()
 
 
 class Limuzina(Auto):
-    def __init__(self, naziv, kapacitet,polaziste, odrediste, cijena):
-        '''nasljeđuje klasu Ucenik, ali dodaje rječnik matura koji ima oblik {'naziv predmeta na maturi':'razina A ili B'}'''
-        super().__init__(naziv,kapacitet)
-        self.p=polaziste
-        self.o=odrediste
-        self.c=cijena
+    def __init__(self, broj_kotača, namjena, dužina, boja):
+ 
+        super().__init__(broj_kotača, namjena)
+        self.d=dužina
+        self.b=boja
     def __repr__(self):
-        return f'Izlet:{self.n} ({self.k},{self.p}-{self.o},{self.c})'
+        return f'Limuzina broj  kotača: {self.k} namjena: {self.n} dužina:{self.d} boja:{self.b}'
     def __str__(self):
         return self.__repr__()
