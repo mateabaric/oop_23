36e601b6e75c9e90aa898441ce077626f45d3a52
mateabaric <131155225+mateabaric@users.noreply.github.com>
2024-10-24 09:01:55 +0200

    Update and rename aktivnosti.py to auti.py

--- klase/auti.py
+++ klase/auti.py
@@ -1,4 +1,4 @@
-class Aktivnost:
+class Auto:
     def __init__(self,naziv, kapacitet):
         '''klasa za aktivnosti'''
         self.n=naziv
@@ -8,7 +8,7 @@ class Aktivnost:
     def __str__(self):
         return self.__repr__()
 
-class Razgledavanje(Aktivnost):
+class Pickup(Auto):
     def __init__(self, naziv, kapacitet,grad, cijena):
         '''nasljeđuje klasu Aktivnost'''
         super().__init__(naziv,kapacitet)
@@ -20,7 +20,20 @@ class Razgledavanje(Aktivnost):
         return self.__repr__()
 
 
-class Izlet(Aktivnost):
+class SUV(Auto):
+    def __init__(self, naziv, kapacitet,polaziste, odrediste, cijena):
+        '''nasljeđuje klasu Ucenik, ali dodaje rječnik matura koji ima oblik {'naziv predmeta na maturi':'razina A ili B'}'''
+        super().__init__(naziv,kapacitet)
+        self.p=polaziste
+        self.o=odrediste
+        self.c=cijena
+    def __repr__(self):
+        return f'Izlet:{self.n} ({self.k},{self.p}-{self.o},{self.c})'
+    def __str__(self):
+        return self.__repr__()
+
+
+class Limuzina(Auto):
     def __init__(self, naziv, kapacitet,polaziste, odrediste, cijena):
         '''nasljeđuje klasu Ucenik, ali dodaje rječnik matura koji ima oblik {'naziv predmeta na maturi':'razina A ili B'}'''
         super().__init__(naziv,kapacitet)
