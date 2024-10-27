6af730d784da0e9b21ed3e99fd5503f06d6bf982
mateabaric <131155225+mateabaric@users.noreply.github.com>
2024-10-27 15:15:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -44,6 +44,43 @@ def test_busevi():
     b=Button(p,text='PRETRAZI',command=lambda:trazi_busevi(e,izbor))
     b.grid(row=3,column=0)
 
+def trazi_kamioni(e,izbor):
+    L_šlep=[
+        Šleper(12,'prijevoz prehrambenih proizvoda','hrana','hladnjača'),
+        Šleper(14,'prijevoz teške opreme','čelik','s platformom')]
+    L_autotrans=[
+        Autotransporter(13.5,'prijevoz rabljenih vozila',1,'otvorena'),
+        Autotransporter(16,'prijevoz luksuznih vozila',2,'zatvorena'),
+        Autotransporter(10,'prijevoz motocikala',2,'s rampom'),]
+    dužina=int(e.get())
+
+    novi=Toplevel(p)
+    Label(novi,text='Rezultati pretraživanja:').pack()
+    if izbor.get()==1:
+        for i in range(len(L_šlep)):
+            if L_šlep[i].d>=dužina:
+                Label(novi,text=L_šlep[i]).pack()
+    else:
+        for i in range(len(L_turist)):
+            if L_turist[i].k>=kapacitet:
+                Label(novi,text=L_izl[i]).pack()
+    novi.mainloop()
+
+def test_busevi():
+    l=Label(p,text='Unesi podatke o busu')
+    l.grid(row=0,column=0)
+    l=Label(p,text='Vrsta busa:')
+    l.grid(row=1,column=0)
+    izbor=IntVar()
+    Radiobutton(p,variable=izbor,text='Gradski',value=1).grid(row=1,column=1)
+    Radiobutton(p,variable=izbor,text='Turistički',value=2).grid(row=1,column=2)
+    l=Label(p,text='Broj osoba:')
+    l.grid(row=2,column=0)
+    e=Entry(p, width=20)
+    e.grid(row=2,column=1)
+    b=Button(p,text='PRETRAZI',command=lambda:trazi_busevi(e,izbor))
+    b.grid(row=3,column=0)
+
 
 if __name__=='__main__':
     p=Tk()
